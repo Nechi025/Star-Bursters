bb3e00de97f9b79dc945e8ef1873c811196715e3
Nechi025 <ignacioaloswol@hotmail.com>
2024-11-05 17:08:01 -0300

    EnemySpawner

---
+++ Assets/Scripts/Gameplay/Enemies/EnemySpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float timeToStartSpawning;
+    [SerializeField] float timeBetweenSpawning;
+
+    bool readyToSpawn;
+    float timer;
+
+    private void Start()
+    {
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount >= 1)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                timer += Time.deltaTime;
+                if (timeToStartSpawning < timer)
+                {
+                    readyToSpawn = true;
+                    timer = 0;
+                }
+
+                if (readyToSpawn && timer > timeBetweenSpawning)
+                {
+                    timer = 0;
+                    PhotonNetwork.Instantiate(enemyPrefab.name, new Vector2(Random.Range(-4, 4), Random.Range(-4, 4)), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
